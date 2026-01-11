d3797bc48777101753b3ab4dc6bd1354e9ae389c
bhlindemann <bh@lindemannrock.com>
2026-01-11 21:51:19 +0400

    fix: update composer.json to include craft-plugin-base and refactor Settings model

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -10,6 +10,8 @@ namespace lindemannrock\formieparagraphfield\models;
 
 use Craft;
 use craft\base\Model;
+use lindemannrock\base\traits\SettingsConfigTrait;
+use lindemannrock\base\traits\SettingsDisplayNameTrait;
 
 /**
  * Formie Paragraph Field Settings Model
@@ -20,10 +22,12 @@ use craft\base\Model;
  */
 class Settings extends Model
 {
+    use SettingsConfigTrait;
+    use SettingsDisplayNameTrait;
     /**
-     * @var string|null The public-facing name of the plugin
+     * @var string The public-facing name of the plugin
      */
-    public ?string $pluginName = 'Formie Paragraph Field';
+    public string $pluginName = 'Formie Paragraph Field';
 
     /**
      * @var string Default text size (textXS, textSM, textBase, textLG, textXL)
@@ -121,14 +125,10 @@ class Settings extends Model
     }
 
     /**
-     * Check if a setting is overridden in config file
-     *
-     * @param string $setting
-     * @return bool
+     * Plugin handle for config file resolution
      */
-    public function isOverriddenByConfig(string $setting): bool
+    protected static function pluginHandle(): string
     {
-        $configFileSettings = Craft::$app->getConfig()->getConfigFromFile('formie-paragraph-field');
-        return isset($configFileSettings[$setting]);
+        return 'formie-paragraph-field';
     }
 }
